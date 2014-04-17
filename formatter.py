ec73fddfbe213c8d6db7f61d9149426a847e6c55
Yan <yans@yancomm.net>
2014-04-16 21:36:44 -0700

    merged my formatting code with Amat's. It's now OO!

--- formatter.py
+++ formatter.py
@@ -2,146 +2,159 @@
 
 import logging
 l = logging.getLogger("puppeteer.formatter")
+l.setLevel(logging.DEBUG)
 
 import struct
-import math
 
-#import operator
-#def chunk(writes, word_size=4, chunk_size=1):
-#	'''
-#	Splits a bunch of writes into different chunks
-#
-#	Note: I *think* it's little-endian specific
-#
-#	@param writes: a list of (target, value) locations (of size word_size) to overwrite
-#	@param word_size: the word size (in bytes) of the architecture (default: 4)
-#	@param chunk_size: the size (in bytes) of the desired write chunks (default: 1)
-#	'''
-#	byte_writes = []
-#	offsets = range(8 * word_size, -1, -8 * chunk_size)[1:]
-#	mask_piece = int("FF" * chunk_size, 16)
-#
-#	# it's a pain to pass a single write as tuples, so let's handle that
-#	if type(writes[0]) in (int, long):
-#		writes = (writes,)
-#
-#	for target, value in writes:
-#		for offset in offsets:
-#			# Masking and shifting; int is necessary to prevent longs
-#			mask = mask_piece << offset
-#			masked = int((value & mask) >> offset)
-#			byte_writes.append((target + offset/8, masked, chunk_size))
-#
-#	return sorted(byte_writes, key=operator.itemgetter(1))
+class FmtStr(object):
+	def __init__(self, arch, word_offset=None, max_length=None, byte_offset=None, num_written=0, prefix="", pad_length=None, pad_round=None, pad_char='_'):
+		self.arch = arch
+		self.offset = word_offset * self.arch.bytes if byte_offset is None else byte_offset
+		self.prefix = prefix
+		self.pad_length = pad_length
+		self.pad_round = pad_round
+		self.pad_char = pad_char
+		self.max_length = max_length
 
+		self.absolute_reads = [ ]
+		self.absolute_writes = [ ]
+		self.relative_reads = [ ]
+		self.pointed_writes = [ ]
 
-def pad_to_offset(byte_offset, word_size=4):
-	'''
-	Pads the format string to the given offset.
+		# in-progress building
+		self._fmt = ""
+		self._idx = 1
+		self._printed = num_written
+		self._undos = [ ]
 
-	@param byte_offset: the number of bytes to pad the string
-	@param word_size: the word size (in bytes) of the architecture (default: 4)
-	'''
-	word_offset = byte_offset / word_size
-	fmt = "A" * (-byte_offset % word_size)
+	def absolute_write(self, addr, buff):
+		self.absolute_writes.append((addr, buff))
+		return self
 
-	# The format_string was padded
-	if fmt:
-		word_offset += 1
+	def absolute_read(self, addr):
+		self.absolute_reads.append(addr)
+		return self
 
-	return fmt, word_offset
+	def relative_read(self, offset=1, count=1):
+		self.relative_reads.append((offset, count))
+		return self
 
-def offset_read(offset, read_pad, max_length=None, max_offset=None, round_to=None, pad_with="_"):
-	'''
-	Builds an format string for reading at a specified offset.
+	def pointed_write(self, offset, buff):
+		self.pointed_writes.append((offset, buff))
+		return self
 
-	@param offset: the offset to start reading at
-	@param read_pad: the number in the %08x part
-	@param max_read: the maximum number of bytes that can be read (TODO)
-	@param max_length: the maximum length of the format string
-	@param max_offset: the maximum offset to expect, to make padding easier (and avoid stack movement)
-	@param round_to: alternatively, round the size of the format string up to this many bytes
-	@param round_with: the character to use for padding (default '_')
-	'''
-	if max_offset is not None:
-		offset_fmt = "%0" + ("%dd" % int(math.ceil(math.log(max_offset, 10))))
-		#offset_str = offset_fmt % offset
-	else:
-		offset_fmt = "%d"
+	#
+	# Internal functions below
+	#
 
-	fmt = "%%" + offset_fmt + "$0%dx" % (read_pad)
-	num_reads = max_length / len(fmt) if max_length is not None else 1
-	if offset == 0:
-		num_reads -= 1
-		offset += 1
-		fmt *= num_reads
-		fmt = "%%" + ("0%dx" % (read_pad)) + fmt
-	else:
-		fmt *= num_reads
+	def _pad_start(self):
+		'''
+		Pads the start of the format string to the proper offset.
+		'''
+		self._fmt = self.prefix
+		self._fmt += self.pad_char * ((-(self.offset + len(self._fmt))) % self.arch.bytes)
+		self._pad_to_offset()
+		self._printed = len(self._fmt)
 
-	#print "OFFSETS:", range(offset, offset + num_reads)
-	fmt = fmt % tuple(range(offset, offset + num_reads))
+	def _pad_to_offset(self):
+		topad = (-(len(self._fmt) + self.offset)) % self.arch.bytes
+		self._fmt += self.pad_char * topad
+		self._printed += topad
 
-	if max_length is not None:
-		fmt *= max_length / len(fmt)
+	def _pad_end(self):
+		'''
+		Pads the end of format string to the proper offset.
+		'''
+		if self.pad_round is not None:
+			topad = -len(self._fmt) % self.pad_round
+		elif self.pad_length is not None:
+			topad = -len(self._fmt) % self.pad_length
+		else:
+			topad = 0
 
-	if round_to is not None:
-		fmt += pad_with * (round_to - (len(fmt) % round_to))
+		self._fmt += self.pad_char * topad
+		self._printed += topad
 
-	return fmt
+	def _next_offset(self):
+		'''
+		Returns the offset of the next word of the string.
+		'''
+		l.debug("... calculating offset (%d + %d)/%d", self.offset, len(self._fmt), self.arch.bytes)
+		return (self.offset + len(self._fmt)) / self.arch.bytes + 1
 
-def absolute_string(byte_offset, writes=None, reads=None, read_length = None, current_length=0, pad_to=0, pad_with="_", word_size=4, endness="<"):
-	'''
-	Builds the whole format string
+	def _undo(self):
+		self._fmt, self._idx = self._undos.pop()
 
-	@param writes: a list of (addr, content_as_bytes) tuples to overwrite
-	@param reads: alternatively, a list of memory addresses to read from
-	@param read_length: the length to limit reads to
-	@param byte_offset: the offset in bytes on the stack to the format string
-	@param current_length: the length of the format string prefix (if there is one)
-	@param pad_to: the size of the format string to generate
-	@param pad_with: what character to pad with
-	@param word_size: the word size of the architecture
-	'''
-	l.debug("Starting format string build...")
-	format_start, word_offset = pad_to_offset(byte_offset, word_size=word_size)
+	def _checkpoint(self):
+		self._undos.append((self._fmt, self._idx))
 
-	if writes is not None:
-		format_start += "".join(struct.pack(endness + "I", t) for t, _ in writes)
-	elif reads is not None:
-		format_start += "".join(struct.pack(endness + "I", t) for t in reads)
-	format_end = ""
+	def _do_relative_read(self, offset=0):
+		self._checkpoint()
 
-	if writes is not None:
-		current_length += len(format_start)
+		sized_x = "0%dx" % (self.arch.bytes*2)
+		if offset == self._idx:
+			self._fmt += "%%%s" % sized_x
+			self._idx += 1
+		else:
+			self._fmt += '%' + str(offset) + '$' + sized_x
+
+	def _do_relative_reads(self):
+		for offset,count in self.relative_reads:
+			for i in range(count):
+				self._do_relative_read(offset + i)
+
+	def _do_absolute_writes(self):
+		self._checkpoint()
+
+		self._pad_to_offset()
+		addr_offset = self._next_offset()
+		l.debug("... initial addr offset: %d", addr_offset)
+		self._fmt += "".join(struct.pack(self.arch.struct_fmt, t) for t, _ in self.absolute_writes)
+		self._printed += self.arch.bytes * len(self.absolute_writes)
 
 		modifiers = { 1: "hh", 2: "h", 4: "", 8: "ll" }
 		struct_fmts = { 1: "B", 2: "H", 4: "I", 8: "Q" }
-		for t, c in writes:
+		for n,(t,c) in enumerate(self.absolute_writes):
 			l.debug("... adding write to 0x%x of size %d", t, len(c))
-			v = struct.unpack(endness + struct_fmts[len(c)], c)[0]
-			next_length = (v - current_length) % (256 ** len(c))
-
+			v = struct.unpack(self.arch.endness + struct_fmts[len(c)], c)[0]
+			next_length = (v - self._printed) % (256 ** len(c))
+	
 			# For 4 and less characters, printing directly is more efficient
 			# For 5 to 8, the general method can't be used
 			# Otherwise, use general method
 			if next_length < 5:
-				format_end += "A" * next_length
+				self._fmt += self.pad_char * next_length
 			elif next_length < 8:
-				format_end += "%{:d}hhx".format(next_length)
+				self._fmt += "%{:d}hhx".format(next_length)
 			else:
-				format_end += "%{:d}x".format(next_length)
-			current_length += next_length
+				self._fmt += "%{:d}x".format(next_length)
+			self._printed += next_length
+	
+			self._fmt += "%{:d}${:s}n".format(addr_offset + n, modifiers[len(c)])
+	
+	def _do_absolute_reads(self):
+		self._checkpoint()
 
-			format_end += "%{:d}${:s}n".format(word_offset, modifiers[len(c)])
-			word_offset += 1
-	elif reads is not None:
-		for t in reads:
-			length_str = ".%d"%read_length if read_length is not None else ""
-			format_end += "%{:d}${:s}s".format(word_offset, length_str)
-			word_offset += 1
+		self._pad_to_offset()
+		addr_offset = self._next_offset()
+		self._fmt += "".join(struct.pack(self.arch.struct_fmt, t) for t in self.absolute_reads)
+		self._printed += self.arch.bytes * len(self.absolute_reads)
 
-	# Pad and return the built format string
-	fmt = format_start + format_end
-	l.debug("... created format strng |%s| of length %s.", fmt, len(fmt))
-	return fmt + pad_with * (pad_to - len(fmt))
+		for n in range(len(self.absolute_reads)):
+			self._fmt += "%{:d}$s".format(addr_offset + n)
+	
+	def build(self):
+		l.debug("Starting format string build...")
+		self._pad_start()
+
+		# carry out the actions. Absolute reads are last because we can't predict their output.
+		self._do_relative_reads()
+		self._do_absolute_writes()
+		self._do_absolute_reads()
+
+		while self.max_length is not None and len(self._fmt) > self.max_length:
+			self._undo()
+		self._pad_end()
+
+		l.debug("... created format strng |%s| of length %s.", repr(self._fmt), len(self._fmt))
+		return self._fmt
